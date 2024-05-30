1f897b44e361b7ffe62a54e7fdd2a009e545575b
davidfq <davidfq@gmail.com>
2024-05-30 19:15:07 +0200

    Add modules: auth and tenant API script

---
+++ modules/psoxy_connection/main.tf
@@ -0,0 +1,29 @@
+resource "local_file" "worklytics_tenant_api_script_file" {
+  content = <<EOT
+#!/bin/bash
+
+# Script to set up a Psoxy connection for ${var.psoxy_connection.integration}
+
+TOKEN=`aws cognito-identity get-open-id-token-for-developer-identity \
+  --identity-pool-id ${var.identity_pool_id} \
+  --logins ${var.tenant_api_host}=${var.identity_pool_user_principal} \
+  --region ${var.identity_pool_region} \
+  | jq -r '.Token'`
+
+curl -X POST https://${var.tenant_api_host}/tenant-api/data-connections \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "x-worklytics-tenant-id: ${var.worklytics_tenant_id}"\
+  -H "Content-Type: application/json" \
+  -d '{
+       "integrationId": "${var.psoxy_connection.integration}",
+        "settings": {
+          "PROXY_DEPLOYMENT_KIND": "${var.psoxy_connection.kind}",
+          "PROXY_ENDPOINT": "${var.psoxy_connection.endpoint}",
+          "PROXY_AWS_REGION": "${var.psoxy_connection.region}",
+          "PROXY_AWS_ROLE_ARN": "${var.psoxy_connection.role_arn}"
+        }
+     }'
+EOT
+  filename = "${coalesce(var.psoxy_connection_script_path, path.module)}/create_${var.psoxy_connection.integration}_connection.sh"
+  file_permission = "0755"
+}
---
+++ modules/psoxy_connection/variables.tf
@@ -0,0 +1,46 @@
+variable "worklytics_tenant_id" {
+  type        = string
+  description = "Numeric ID of your Worklytics tenant's service account (obtain from Worklytics Web App)."
+
+  validation {
+    condition     = var.worklytics_tenant_id == null || can(regex("^\\d{21}$", var.worklytics_tenant_id))
+    error_message = "`worklytics_tenant_id` must be a 21-digit numeric value. (or `null`, for pre-production use case where you don't want external entity to be allowed to assume the role)."
+  }
+}
+
+variable "tenant_api_host" {
+  type        = string
+  description = "Host of the Worklytics Tenant API"
+  default     = "intl.worklytics.co"
+}
+
+variable "identity_pool_id" {
+  type        = string
+  description = "The ID of the Cognito Identity Pool to use for authentication with the Worklytics Tenant API."
+}
+
+variable "identity_pool_user_principal" {
+  type        = string
+  description = "The user principal that has been granted access to the Worklytics Tenant API."
+}
+
+variable "identity_pool_region" {
+  type        = string
+  description = "The region of the Cognito Identity Pool to use for authentication with the Worklytics Tenant API."
+}
+
+variable "psoxy_connection" {
+  type = object({
+    integration = string
+    kind        = string
+    endpoint    = string
+    region      = string
+    role_arn    = string
+  })
+  description = "The connection details for a Psoxy connection to be created via Worklytics Tenant API."
+}
+
+variable "psoxy_connection_script_path" {
+  type = string
+  description = "Where to create the script to create the Psoxy connection"
+}
